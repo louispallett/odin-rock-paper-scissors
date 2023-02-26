138a8192346610505fcd59702ac94ff60b4b79be
louispallett <louis.pallett@outlook.com>
2023-02-26 23:55:28 +0000

    Added loop to play game 5 times

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Part of The Odin Project Foundations course. Create a game of "rock, paper, scis
 26.02.23 - Prompted user to give result (ensuring this is not case sensitive) and directed the computer to give a random choice of "Rock", "Paper", or "Scissors".
 
 26.02.23 (22:30) - Added play round function. Cleaned up code created early to ensure the round works (redefining variables so not using functions as parameters).
+
+26.02.23 (23:54) - Added loop to play 5 times.
--- index.js
+++ index.js
@@ -1,79 +1,104 @@
-//take in the user's choice
-let userChoice = prompt("Rock, Paper, or Scissors?", "Rock");
-
-//ensure users choice is not case sensitive (i.e. rock or Rock or rOck is accepted)
-function userChoiceCapitalize (userChoice)
+//Create a function to play the game in 5 rounds
+function game(numOfRounds)
 {
-    return userChoice.charAt(0).toUpperCase() + userChoice.slice(1).toLowerCase();
-}
+    numOfRounds = numOfRounds || 0;
+    let i;
+    for(i = 1; i < numOfRounds; i++)
+    {
 
-//print userChoice (check)
-userChoice = userChoiceCapitalize(userChoice);
-console.log(userChoice);
+        //take in the user's choice
+        let userChoice = prompt("Rock, Paper, or Scissors?", "Rock");
 
-//randomly generate the computer's choice
-
-    //Pseudocode:
-    //We need to define three string values.
-
-    let computerChoice = ["Rock", "Paper", "Scissors"];
-
-        function getComputerChoice()
+        //ensure users choice is not case sensitive (i.e. rock or Rock or rOck is accepted)
+        function userChoiceCapitalize (userChoice)
         {
-            
-            //We need to select one of those at random
-            let randomiseComputerChoice = Math.floor(Math.random() * computerChoice.length) + 1;
+            return userChoice.charAt(0).toUpperCase() + userChoice.slice(1).toLowerCase();
+        }
 
-            //We need to define the array numbers as strings
-            if (randomiseComputerChoice === 1)
+        //print userChoice (check)
+        userChoice = userChoiceCapitalize(userChoice);
+        console.log(userChoice);
+
+        //randomly generate the computer's choice
+
+            //Pseudocode:
+            //We need to define three string values.
+
+            let computerChoice = ["Rock", "Paper", "Scissors"];
+
+                function getComputerChoice()
+                {
+                    
+                    //We need to select one of those at random
+                    let randomiseComputerChoice = Math.floor(Math.random() * computerChoice.length) + 1;
+
+                    //We need to define the array numbers as strings
+                    if (randomiseComputerChoice === 1)
+                    {
+                        return "Rock";
+                    } else if (randomiseComputerChoice === 2)
+                    {
+                        return "Paper";
+                    } else if (randomiseComputerChoice === 3)
+                    {
+                        return "Scissors";
+                    }
+                }
+            //We need to print out the getComputerChoice() function
+            
+            computerChoice = (getComputerChoice());
+            console.log(computerChoice);
+            
+
+        //set out rules: rock beats scissors, scissors beats paper, paper beats rock
+        //test result based on player choice and computer's choice
+
+        function playRound (userChoice, computerChoice)
+        {
+            if (userChoice === "Paper" && computerChoice === "Rock")
             {
-                return "Rock";
-            } else if (randomiseComputerChoice === 2)
+                return("Paper beats rock, you won!");
+            } else if (userChoice === "Paper" && computerChoice === "Scissors")
             {
-                return "Paper";
-            } else if (randomiseComputerChoice === 3)
+                return("Scissors beats paper, you lost!");
+            } else if (userChoice === "Scissors" && computerChoice === "Rock")
             {
-                return "Scissors";
+                return("Rock beats scissors, you lost!");
+            } else if (userChoice === "Scissors" && computerChoice === "Paper")
+            {
+                return("Scissors beats paper, you won!");
+            } else if (userChoice === "Rock" && computerChoice === "Paper")
+            {
+                return("Paper beats rock, you lost!");
+            } else if (userChoice === "Rock" && computerChoice === "Scissors")
+            {
+                return("Rock beats scissors, you won!");
+            } else 
+            {
+                return("You tied!");
             }
         }
-    //We need to print out the getComputerChoice() function
-    
-    computerChoice = (getComputerChoice());
-    console.log(computerChoice);
-    
 
-//set out rules: rock beats scissors, scissors beats paper, paper beats rock
-//test result based on player choice and computer's choice
+        //return result
+        console.log(playRound(userChoice, computerChoice));  
 
-//return result
-function playRound (userChoice, computerChoice)
-{
-    if (userChoice === "Paper" && computerChoice === "Rock")
-    {
-        return("Paper beats rock, you won!");
-    } else if (userChoice === "Paper" && computerChoice === "Scissors")
-    {
-        return("Scissors beats paper, you lost!");
-    } else if (userChoice === "Scissors" && computerChoice === "Rock")
-    {
-        return("Rock beats scissors, you lost!");
-    } else if (userChoice === "Scissors" && computerChoice === "Paper")
-    {
-        return("Scissors beats paper, you won!");
-    } else if (userChoice === "Rock" && computerChoice === "Paper")
-    {
-        return("Paper beats rock, you lost!");
-    } else if (userChoice === "Rock" && computerChoice === "Scissors")
-    {
-        return("Rock beats scissors, you won!");
-    } else 
-    {
-        return("You tied!");
+        //return number of rounds played
+        function roundNumber(i)
+        {
+            if (i === 1)
+            {
+                return("You've played 1 time!");
+            } else 
+            {
+                return("You've played " + i + " times!");
+            }
+        }
+
+        console.log(roundNumber(i));
+        
     }
+    
 }
 
-console.log(playRound(userChoice, computerChoice));
-
-
-
-
+//Call function (inserting no of round (-1))
+game(6);
