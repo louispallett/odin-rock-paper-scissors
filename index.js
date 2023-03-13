5924c8e64ebb82ace23042177d762f538835436a
louispallett <louis.pallett@outlook.com>
2023-03-13 08:39:13 +0000

    Finalized CSS and user buttons now working

--- index.js
+++ index.js
@@ -1,96 +1,79 @@
 let userScore = 1;
 let computerScore = 1;
+let userChoice;
+
+//User selection variables
+const rockButton = document.querySelector("#Rock");
+const paperButton = document.querySelector("#Paper");
+const scissorsButton = document.querySelector("#Scissors");
+
+//take in the user's choice
+rockButton.addEventListener('click', (e) => {
+    console.log(playRound(e.target.id, computerChoice));
+})
+
+paperButton.addEventListener('click', (e) => {
+    console.log(playRound(e.target.id, computerChoice));
+})
+
+scissorsButton.addEventListener('click', (e) => {
+    console.log(playRound(e.target.id, computerChoice));
+})
+
 
 //Create a function to play the game in 5 rounds
-function game(numOfRounds)
-{
-    
-    
-    numOfRounds = numOfRounds || 0;
-    let i;
-    for(i = 1; i < numOfRounds; i++)
-    {
+    let computerChoice = ["Rock", "Paper", "Scissors"];
 
-        //take in the user's choice
-        let userChoice = prompt("Rock, Paper, or Scissors?", "Rock");
-
-        //ensure users choice is not case sensitive (i.e. rock or Rock or rOck is accepted)
-        function userChoiceCapitalize (userChoice)
+        function getComputerChoice()
         {
-            return userChoice.charAt(0).toUpperCase() + userChoice.slice(1).toLowerCase();
-        }
-
-        //print userChoice (check)
-        userChoice = userChoiceCapitalize(userChoice);
-        console.log(userChoice);
-
-        //randomly generate the computer's choice
-
-            //Pseudocode:
-            //We need to define three string values.
-
-            let computerChoice = ["Rock", "Paper", "Scissors"];
-
-                function getComputerChoice()
-                {
-                    
-                    //We need to select one of those at random
-                    let randomiseComputerChoice = Math.floor(Math.random() * computerChoice.length) + 1;
-
-                    //We need to define the array numbers as strings
-                    if (randomiseComputerChoice === 1)
-                    {
-                        return "Rock";
-                    } else if (randomiseComputerChoice === 2)
-                    {
-                        return "Paper";
-                    } else if (randomiseComputerChoice === 3)
-                    {
-                        return "Scissors";
-                    }
-                }
-            //We need to print out the getComputerChoice() function
             
-            computerChoice = (getComputerChoice());
-            console.log(computerChoice);
-            
+            //We need to select one of those at random
+            let randomiseComputerChoice = Math.floor(Math.random() * computerChoice.length) + 1;
 
-            
-        //set out rules: rock beats scissors, scissors beats paper, paper beats rock
-        //test result based on player choice and computer's choice
-        function playRound ()
-        {
-            if (userChoice === "Paper" && computerChoice === "Rock")
+            //We need to define the array numbers as strings
+            if (randomiseComputerChoice === 1)
             {
-                return("Paper beats rock, you won! Your score is " + (userScore++));
-            } else if (userChoice === "Paper" && computerChoice === "Scissors")
+                return "Rock";
+            } else if (randomiseComputerChoice === 2)
             {
-                return("Scissors beats paper, you lost! The computer's score is " + (computerScore++));
-            } else if (userChoice === "Scissors" && computerChoice === "Rock")
+                return "Paper";
+            } else if (randomiseComputerChoice === 3)
             {
-                return("Rock beats scissors, you lost! The computer's score is " + (computerScore++));
-            } else if (userChoice === "Scissors" && computerChoice === "Paper")
-            {
-                return("Scissors beats paper, you won! Your score is " + (userScore++));
-            } else if (userChoice === "Rock" && computerChoice === "Paper")
-            {
-                return("Paper beats rock, you lost! The computer's score is " + (computerScore++));
-            } else if (userChoice === "Rock" && computerChoice === "Scissors")
-            {
-                return("Rock beats scissors, you won! Your score is " + (userScore++));
-            } else 
-            {
-                return("You tied!");
+                return "Scissors";
             }
         }
-        
-        //return result
-        console.log(playRound());  
+    
+//set out rules: rock beats scissors, scissors beats paper, paper beats rock
+//test result based on player choice and computer's choice
+function playRound (userChoice, computerChoice)
+{
+    computerChoice = (getComputerChoice());
+    if (userScore < 6 && computerScore < 6)
+    {
+        if (userChoice === "Paper" && computerChoice === "Rock")
+        {
+            return("Paper beats rock, you won! Your score is " + (userScore++));
+        } else if (userChoice === "Paper" && computerChoice === "Scissors")
+        {
+            return("Scissors beats paper, you lost! The computer's score is " + (computerScore++));
+        } else if (userChoice === "Scissors" && computerChoice === "Rock")
+        {
+            return("Rock beats scissors, you lost! The computer's score is " + (computerScore++));
+        } else if (userChoice === "Scissors" && computerChoice === "Paper")
+        {
+            return("Scissors beats paper, you won! Your score is " + (userScore++));
+        } else if (userChoice === "Rock" && computerChoice === "Paper")
+        {
+            return("Paper beats rock, you lost! The computer's score is " + (computerScore++));
+        } else if (userChoice === "Rock" && computerChoice === "Scissors")
+        {
+            return("Rock beats scissors, you won! Your score is " + (userScore++));
+        } else 
+        {
+            return("You tied!");
+        }
     }
 }
-
-//Call function to initialize game
-game();
 
 //calculate final result
 function finalResult()
@@ -108,5 +91,18 @@ function finalResult()
 }
 
 //Return final result and verdict
-console.log(finalResult());
+//console.log(finalResult());
+
+/* Creating the UI for the website:
+
+Tasks:
+    Allow the userChoice to equal what the user selects.
+        If the user presses this, userChoice is equal to this (i.e. rock button user choice is "rock")
+    
+    Display compChoice
+        Should ONLY display choice once selection is made (and by extension, when results are displayed)
+    
+    Display result of round (and round number)
+    
+    Display final result.*/
 
